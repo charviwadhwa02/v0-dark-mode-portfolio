cfab712493650027c98111cec3da2e4bacffbd65
charviwadhwa02 <charviwadhwa02@gmail.com>
2025-07-03 02:30:24 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Charvi Wadhwa",
+icons: {
+            icon: '/favicon.ico', // Path to your favicon
+          },
  
 }
 
